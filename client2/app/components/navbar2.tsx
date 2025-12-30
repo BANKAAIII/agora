1c9e88c4929f8a46161a2cbda8d3e32566901567
BANKAAIII <ameyaswe.com>
2025-12-30 18:56:25 +0530

    images

--- client2/app/components/navbar2.tsx
+++ client2/app/components/navbar2.tsx
@@ -2,12 +2,8 @@
 
 import React from 'react'
 import { navBarInterface } from '../utils/NavBarInterface';
-import aossie from "../public/aossie.png";
-import NightMode from "../public/night-mode.png";
-import wallet from "../public/wallet.png";
 import Image from 'next/image';
 import { useTheme } from '../themeProvider';
-import searchLogo from "../public/search.png";
 
 const NavBar2 = ({ className, onClick1, onClick2, open, setOpen,pathName }: navBarInterface) => {
   const { toggleTheme } = useTheme();
@@ -22,7 +18,7 @@ const NavBar2 = ({ className, onClick1, onClick2, open, setOpen,pathName }: navB
          {/* logo */}
          <div className="flex flex-row pl-[25px] md:pl-[25px] xl:pl-[32px] gap-[19px] items-center justify-center ">
         <Image
-          src={aossie}
+          src={"/aossie.png"}
           alt="loading"
           className="w-[40px] h-[40px] sm:h-[50px] sm:w-[50px] xl:h-[74px] xl:w-[74px]"
         />
@@ -37,14 +33,14 @@ const NavBar2 = ({ className, onClick1, onClick2, open, setOpen,pathName }: navB
       <div className="pr-[15px] md:pr-[25px] lg:pr-[32px] gap-x-[5px] md:gap-x-[20px] flex flex-row items-center justify-center">
        <div className="flex flex-row items-center justify-end p-4 w-[500px] h-[60px] rounded-full bg-TitleDot/46  " >
         <input className=" flex w-full outline-none p-5 " placeholder='Search '/>
-        <Image src={searchLogo} alt="loading" className="invert dark:invert-0 p-0.5  w-[30px] h-[30px] " />
+        <Image src={"/search.png"} alt="loading" className="invert dark:invert-0 p-0.5  w-[30px] h-[30px] " />
        </div>
        <button
           className="h-[25px] w-[40px] h-[40px] sm:w-[50px] sm:h-[50px] xl:w-[60px] xl:h-[60px] bg-[#F4F4F4]  rounded-[15px] flex items-center justify-center"
           onClick={() => setOpen(!open)}
         >
           <Image
-            src={wallet}
+            src={"/wallet.png"}
             alt="wallet"
             className=" w-[25px] md:h-[25px] md:w-[25px] xl:h-[36px] xl:w-[36px]"
           />
@@ -56,7 +52,7 @@ const NavBar2 = ({ className, onClick1, onClick2, open, setOpen,pathName }: navB
           onClick={() => setOpen(!open)}
         >
           <Image
-            src={wallet}
+            src={"/wallet.png"}
             alt="wallet"
             className=" w-[25px] md:h-[25px] md:w-[25px] xl:h-[36px] xl:w-[36px]"
           />
